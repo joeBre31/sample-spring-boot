d680303caced0f2bc04fdb766dfa372bb41632ee
Formation <formation@Ubuntu-16.04.2-VBox>
2017-04-28 10:16:12 +0200

    print dans le main

--- src/main/java/com/example/DemoApplication.java
+++ src/main/java/com/example/DemoApplication.java
@@ -2,11 +2,13 @@ package com.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import java.lang;
 
 @SpringBootApplication
 public class DemoApplication {
 
 	public static void main(String[] args) {
+         System.out.println("main sample-spring-boot");
 		SpringApplication.run(DemoApplication.class, args);
 	}
 }
