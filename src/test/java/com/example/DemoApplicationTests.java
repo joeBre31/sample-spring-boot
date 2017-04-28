514a609f4cb002e6a6f4ff424f83a2ff779e1b10
Formation <formation@Ubuntu-16.04.2-VBox>
2017-04-28 15:05:30 +0200

    modif test pour ignore

--- src/test/java/com/example/DemoApplicationTests.java
+++ src/test/java/com/example/DemoApplicationTests.java
@@ -5,6 +5,7 @@ import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import static org.junit.Assert.*;
+import org.junit.Ignore;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -16,6 +17,13 @@ public class DemoApplicationTests {
 
         @Test
         public final void testMultiply() {
+        // TODO
+         fail("Not yet implemented");
+       }
+
+        @Ignore
+        @Test
+        public final void testSkip() {
         // TODO
          fail("Not yet implemented");
        }
