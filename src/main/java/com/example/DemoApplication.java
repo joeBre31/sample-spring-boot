1408e87270ee2b47267b3206d452b0e009f44094
Formation <formation@Ubuntu-16.04.2-VBox>
2017-04-28 11:17:11 +0200

    suppression print dans le main

--- src/main/java/com/example/DemoApplication.java
+++ src/main/java/com/example/DemoApplication.java
@@ -2,13 +2,11 @@ package com.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import java.lang;
 
 @SpringBootApplication
 public class DemoApplication {
 
 	public static void main(String[] args) {
-         System.out.println("main sample-spring-boot");
 		SpringApplication.run(DemoApplication.class, args);
 	}
 }
